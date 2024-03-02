c5b346be31c2835ffceeef8f6878439ea99dd007
shahnawaz <mohammad.shahnawazmalik@infra.market>
2024-03-02 13:24:01 +0530

    fixForLineIssue

--- LaunchEazy/src/components/Footer/index.jsx
+++ LaunchEazy/src/components/Footer/index.jsx
@@ -59,7 +59,7 @@ function Footer() {
         </div>
        </div>
        <div className="lineConatiner">
-        <div className="line"></div>
+        <div className="lineFooter"></div>
        </div>
        <div className="copywrite">
         <div className="leftTextCopywrite">Copyright 2023 Launcheazy AI Private Limited. All rights reserved.</div>
